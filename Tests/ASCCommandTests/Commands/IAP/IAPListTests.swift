ceedcacf0b1428e43d121b030411eb54599335bf
slamhan <slamhan1987@hotmail.com>
2026-04-26 12:50:39 +0800

    test(iap): add commands for review screenshot and images listing

--- Tests/ASCCommandTests/Commands/IAP/IAPListTests.swift
+++ Tests/ASCCommandTests/Commands/IAP/IAPListTests.swift
@@ -31,6 +31,8 @@ struct IAPListTests {
                 "createLocalization" : "asc iap-localizations create --iap-id iap-1 --locale en-US --name <name>",
                 "delete" : "asc iap delete --iap-id iap-1",
                 "getAvailability" : "asc iap-availability get --iap-id iap-1",
+                "getReviewScreenshot" : "asc iap-review-screenshot get --iap-id iap-1",
+                "listImages" : "asc iap-images list --iap-id iap-1",
                 "listLocalizations" : "asc iap-localizations list --iap-id iap-1",
                 "listOfferCodes" : "asc iap-offer-codes list --iap-id iap-1",
                 "listPricePoints" : "asc iap price-points list --iap-id iap-1",
